88204652b4116fe3dcd2d9a826495204ce0c3309
Haezed <130173952+Haezed@users.noreply.github.com>
2025-09-02 19:09:57 -0400

    FEAT - Add variable jump height

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,11 +5,20 @@ extends CharacterBody2D
 # Movement speed
 @export var speed = 300
 
-# Jump height
-@export var jump_force = 700
+# Initial jump force
+@export var jump_force = 500
+
+# Maximum jump height
+@export var max_jump_height = 1000
+
+# Time it takes to reach max jump height
+@export var max_jump_time = 0.4
 
 # Gravity
 @export var gravity = 2000
+
+var jump_time = 0
+var is_jumping = false
 
 func _physics_process(delta):
 	# Apply gravity
@@ -18,7 +27,16 @@ func _physics_process(delta):
 
 	# Handle jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		is_jumping = true
+		jump_time = 0
 		velocity.y = -jump_force
+
+	if Input.is_action_pressed("ui_accept") and is_jumping:
+		if jump_time < max_jump_time:
+			velocity.y -= (max_jump_height / max_jump_time) * delta
+			jump_time += delta
+	else:
+		is_jumping = false
 
 	# Get input direction
 	var direction = Input.get_axis("ui_left", "ui_right")
